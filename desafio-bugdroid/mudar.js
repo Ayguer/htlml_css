2ab2cb70b162968c88b12dd3ca0a0250054dc25e
Ayguer_Acabane <132191421+Ayguer@users.noreply.github.com>
2023-05-05 21:39:35 +0100

    v8

---
+++ desafio-bugdroid/mudar.js
@@ -0,0 +1,15 @@
+let radio= [document.getElementById('radio1'),document.getElementById('radio2'),document.getElementById('radio3'),document.getElementById('radio4')]
+let a= 1
+if(a==4){
+    a=0
+}
+setInterval(function(){
+    
+    radio[a].checked='true'
+    a+=1
+    if(a==4){
+        a=0
+    }
+    
+
+},2000)
